cbba483cf3a003ebbfd71c9f417051d06751144a
vitalikrez <vitalik.rez@gmail.com>
2022-09-23 00:20:18 +0300

    Create description of selected book

--- src/app/book/book.ts
+++ src/app/book/book.ts
@@ -1,4 +1,11 @@
 export class Book {
     id!: number;
     name!: string;
+    author!: string;
+    year!:number;
+    pages!: number;
+    publisher!: string;
+    cover!: string;
+    descriptions!: string;
+
 }
